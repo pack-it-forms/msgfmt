fb3b2c0cf5ac79995842728c24050efc9fe916aa
Keith Amidon <keith@awakenetworks.com>
2018-01-19 06:46:22 -0800

    Pin nixpkgs and sdtpl for release builds
    nixpkgs is pinned to the tip of the 17.09 branch at the time of this
    commit. The intention is to keep it on the most recent stable commit
    at each release.
    
    sdtpl is pinned to the tip of master at the time of this commit. It is
    not expected to change significantly unless changes in dependencies
    will require updates.

--- project-deps.nix
+++ project-deps.nix
@@ -4,8 +4,8 @@ self: super:
   nixpkgs = self.releaseGHDevLocal {
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "TODO";
-    sha256 = "TODO";
+    rev = "66b4de79e3841530e6d9c6baf98702aa1f7124e4";
+    sha256 = "1l3lwi944hnxka0nfq9a1g86xhc0b8hzqr2fm6cvds33gj26l0g4";
     local = <nixpkgs>;
     fetch = super.nixpkgs.fetchFromGitHub;
   } { config = {}; };
@@ -14,8 +14,8 @@ self: super:
     sdtpl = self.releaseGHDevLocal {
       owner = "peter-sa";
       repo = "sdtpl";
-      rev = "TODO";
-      sha256 = "TODO";
+      rev = "bdce14eb68275d4f76a0de80329acb3b2bb0f030";
+      sha256 = "04hr928q3129y3f6725qviwms41j2fwiljqypdgklv973zqcycgz";
       local = ../sdtpl;
     };
   };
